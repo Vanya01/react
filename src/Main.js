ac02463e45e2e520fb4c7bb647515a7ca92fad53
Vanya01 <good.boy.2k19@gmail.com>
2021-08-11 11:19:14 +0300

    2_day_of_studying_react

---
+++ src/Main.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './App.css';
+import './Header.css';
+function App() {
+    return (
+        <main>
+            <article>
+                <header>
+                    <h1>Title_one</h1>
+                    <h2>Title_two</h2>
+                </header>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim eos sint unde vitae! Architecto enim est fugiat hic magni soluta.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim eos sint unde vitae! Architecto enim est fugiat hic magni soluta.</p>
+            </article>
+        </main>
+    );
+}
+export default App;
